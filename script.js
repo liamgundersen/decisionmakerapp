ec8c2b033f6bfef01b43fd989995813c1377694f
liamgundersen <lgundersen28@students.stab.org>
2024-09-25 09:13:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+function findCar(budget, place){
+    var feedback;
+if(place==city&&budget==100){feedback= "Porsche GT3 RS"}
+else if(place==city&&budget==0){feedback= "Hellcat"}
+else if(place==town&&budget==100){feedback= "Lamborgini Urus"}
+else if(place==town&&budget==0){feedback= "Kia"}
+else if(place==beach&&budget==100){feedback= "Ferrari Roma"}
+else if(place==beach&&budget==0){feedback= "Ford Bronco"}
+else if(place==south&&budget==100){feedback= "Hummer"}
+else if(place==south&&budget==0){feedback= "Ford Pickup Truck"}
+return feedback
+}
+
+function updateScreen(){
+
+
+
+}
+
